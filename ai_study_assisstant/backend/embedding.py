4d65d7cee9f80565eb04e04707ad31e49ef8aaa0
ArpanBishnoi <vishnoiarpan61@gmail.com>
2026-08-05 23:10:51 +0530

    Add files via upload

--- ai_study_assisstant/backend/embedding.py
+++ ai_study_assisstant/backend/embedding.py
@@ -2,14 +2,14 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
-print(API_KEY := os.getenv("OPEN_ROUTER_API_KEY"))
+
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPEN_ROUTER_API_KEY"),
 )
 def generate_embedding(text: str) -> list:
     response = client.embeddings.create(
-        model="text-embedding-3-small",
+        model="nvidia/llama-nemotron-embed-vl-1b-v2:free",
         input=text,
     )
     return response.data[0].embedding
