02228857e19a42dfac65ac2a96561ebb8d6e5e12
ArpanBishnoi <vishnoiarpan61@gmail.com>
2026-08-05 22:54:06 +0530

    Add files via upload

--- ai_study_assisstant/backend/embedding.py
+++ ai_study_assisstant/backend/embedding.py
@@ -1,15 +1,16 @@
 import os
 from openai import OpenAI
-
+from dotenv import load_dotenv
+load_dotenv()
+print(API_KEY := os.getenv("OPENROUTER_API_KEY"))
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY"),
+    api_key=os.getenv("OPEN_ROUTER_API_KEY"),
 )
-
-
 def generate_embedding(text: str) -> list:
     response = client.embeddings.create(
         model="text-embedding-3-small",
         input=text,
     )
     return response.data[0].embedding
+
