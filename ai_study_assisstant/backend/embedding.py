2f35dbf9768c8783794157c977301d83a995801e
ArpanBishnoi <vishnoiarpan61@gmail.com>
2026-08-05 22:54:47 +0530

    Refactor embedding.py for improved readability

--- ai_study_assisstant/backend/embedding.py
+++ ai_study_assisstant/backend/embedding.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
-print(API_KEY := os.getenv("OPENROUTER_API_KEY"))
+print(API_KEY := os.getenv("OPEN_ROUTER_API_KEY"))
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPEN_ROUTER_API_KEY"),
